a48cfb77f8c13412bf3e96dfacfa0224ec093a53
Rajan Poudel <raajan.works@gmail.com>
2025-06-07 16:56:25 +0545

    Add files via upload

---
+++ Tribhuvan University (TU) C Programming Solutions/BSc CSIT/squareSum.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{
+	int i,n;
+	printf("Enter The Number: ");
+	scanf("%d",&n);
+	
+	int sum=0;
+	
+	while(i<=n)
+	{
+		sum += i*i;
+		i++;
+	}
+	
+	printf("The Sum Is %d",sum);
+	return 0;
+}
+
+/*
+Write a program that uses “while”loop to compute and prints the sum of a given numbers of squares. 
+For example, if 4 is input, then the program will print 30, which is equal to 12+22+32+42
+*/
