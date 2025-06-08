3b105db36f5b359df49f0a5bc67c96be6e17a042
Rajan Poudel <raajan.works@gmail.com>
2025-06-08 12:04:12 +0545

    Add files via upload

---
+++ Tribhuvan University (TU) C Programming Solutions/BSc CSIT/readCharacterArray .c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<string.h>
+
+int main(){
+	char str[] = "TRIBHUVAN UNIVERSITY";
+	int count=0;
+	int i;
+	 for(i = 0; str[i] != '\0'; i++) {
+        if(str[i] == 'I' || str[i]=='i') {
+            count++;
+        }
+    }
+    
+    printf("The character 'I' occurs %d times in the string.\n", count);
+
+}
+
+/*
+Write a program to read a character array in input as “TRIBHUVAN UNIVERSITY” from the user and find out how 
+many times a character “I” occurs in that array?
+
+*/
